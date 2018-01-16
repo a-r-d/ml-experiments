9dde7a55d5089c8693ae9a473509a5e1a517bc74
ard <aaron@ard.ninja>
2018-01-15 20:12:55 -0600

    blah

--- day-1.py
+++ day-1.py
@@ -1,3 +1,29 @@
-import matplotlib
-import numpy
-import sklearn
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+print(digits.data)
+print(digits.target)
+
+
+#clf = svm.SVC()
+clf = svm.SVC(gamma=0.001, C=100)
+
+# This loads in all but the last 10 data points,
+# so we can use all of these for training.
+# Then, we can use the last 10 data points for testing.
+X,y = digits.data[:-10], digits.target[:-10]
+
+# The X contains all of the "coordinates" and y is simply the "target" or "classification" of the data
+clf.fit(X,y)
+
+# in the tutorial there is only one set of brackets, should be 2.
+print "Predicting:"
+print(clf.predict(digits.data[[-5]]))
+
+# visualize
+print "Viz: "
+print "Actual value: ", digits.target[-5]
+plt.imshow(digits.images[-5], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
--- readme.md
+++ readme.md
@@ -1,9 +1,14 @@
-# install deps
+# install deps for following sci-py tutorials from Sentdex
+
+Youtube: https://www.youtube.com/watch?v=rAdAVcS4aL0&index=4&list=PLQVvvaa0QuDd0flgGphKCej-9jp-QdzZ3
 
 ```bash
+sudo apt-get install python-tk
+
 pip install --upgrade pip
 pip install matplotlib
 pip install numpy
 sudo pip install scipy
-sudo pip install sklearn
+sudo pip install scikit-learn
+sudo pip install pandas
 ```
